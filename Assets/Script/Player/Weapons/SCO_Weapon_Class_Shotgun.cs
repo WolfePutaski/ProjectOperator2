3b3df6f111a508a7f86b8eb56cb782704826b9f8
SuppakitW <43819232+WolfePutaski@users.noreply.github.com>
2021-02-19 21:01:18 +0700

    v 0.3.6
    - Added shotgun shooting and spread.

--- Assets/Script/Player/Weapons/SCO_Weapon_Class_Shotgun.cs
+++ Assets/Script/Player/Weapons/SCO_Weapon_Class_Shotgun.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "SCO_Weapon_Shotgun", menuName = "ScriptableObject_Weapon_Shotgun")]
 public class SCO_Weapon_Class_Shotgun : SCO_Weapon_Master
 {
+    [Header("=== Shotgun Property ===")]
     public int PelletCount;
-    public int Spread;
+    public float Spread;
 }
